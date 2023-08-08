2470131a43de958b695dc685630ca914106da8ec
Jesús Ariel González Bonilla <ariel5253@hotmail.com>
2023-08-08 08:18:03 -0500

    Sesión 08-08

---
+++ 01-Sesión-08-08/Ejercicios/src/main/java/Saludar.java
@@ -0,0 +1,5 @@
+public class Saludar {
+    public static void main(String[] arg){
+        System.out.println("Cordial saludo, buenos días. \n Teams Análsis de Algoritmos")
+    }
+}
---
+++ 01-Sesión-08-08/Ejercicios/src/main/java/Sumar.java
@@ -0,0 +1,13 @@
+public class Sumar {
+    public static void main(String[] args){
+        Double n1;
+        Double n2;
+        Double suma;
+
+        n1 = 10.5;
+        n2 = 2.9;
+        suma = n1+n2;
+
+        System.out.println("La suma es: "+suma);
+    }
+}
